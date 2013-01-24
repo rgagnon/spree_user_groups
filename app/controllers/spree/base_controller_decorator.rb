3c01cd19c1d507b650511c3513446564a1edd3bb
Sean O'Hara <sohara@sohara.com>
2013-01-24 16:58:06 -0500

    Include Spree's ControllerHelpers::Order module so current_order can be found for every controller request

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,9 +1,11 @@
 module Spree
   BaseController.class_eval do
+
+    include Spree::Core::ControllerHelpers::Order
     before_filter :ubiquitous_associate_user
 
     private
-      # it is critical that we don't wait until the checkout process to 
+      # it is critical that we don't wait until the checkout process to
       # attach the user and the order, since we need the cart and
       # the order.total amounts to reflect the user-group-adjusted amounts
       def ubiquitous_associate_user
