88db88fc9dbd7c18b33f57046d3bbf4e6e8b3574
Sean O'Hara <sohara@sohara.com>
2013-01-26 17:36:15 -0500

    Redefine methods in module rather than class eval so that super can be called. Doing so in class_eval was causing recursion (stack too deep) errors when running specs.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,62 +1,25 @@
 module Spree
-  Order.class_eval do
-    def user_group_adjustments
-      adjustments.where(:originator_type => "Spree::UserGroup")
+  # Placing these methods in module and calling super
+  # because alias_method was causing stack too deep errors:
+  # Bascially, methods were calling themselves recursively
+  # when running specs
+  module UserGroupPricing
+
+    def new(*args)
+      super.extend(UserGroupPricing)
     end
 
     # ensure update_prices_per_user is called
     # after every associate_user call
-    #
-    # alias_method :associate_user_without_user_price!, :associate_user!
-
     def associate_user!(user)
-      # associate_user_without_user_price! user
-
-      # Alias method is causing stack level too deep, so for now just
-      # copying over rest of method from Spree Core
-
-      self.user = user
-      self.email = user.email
-      # disable validations since they can cause issues when associating
-      # an incomplete address during the address step
-      save(:validate => false)
-      # --- End of copied from orig class definition ---#
-
+      super
       update_prices_per_user
     end
 
-
     # ensure update_prices_per_user is called
     # after every add_varaint call
-    #
-     alias_method :add_variant_without_user_price, :add_variant
-
     def add_variant(variant, quantity = 1, currency = nil)
-      # current_item = add_variant_without_user_price(variant, quantity, currency)
-
-      # Alias method is causing stack level too deep, so for now just
-      # copying over rest of method from Spree Core
-      current_item = find_line_item_by_variant(variant)
-      if current_item
-        current_item.quantity += quantity
-        current_item.currency = currency unless currency.nil?
-        current_item.save
-      else
-        current_item = LineItem.new(:quantity => quantity)
-        current_item.variant = variant
-        if currency
-          current_item.currency = currency unless currency.nil?
-          current_item.price    = variant.price_in(currency).amount
-        else
-          current_item.price    = variant.price
-        end
-        self.line_items << current_item
-      end
-
-      self.reload
-      current_item
-      # --- End of copied from orig class definition ---#
-
+      current_item = super
       if update_prices_per_user
         current_item.reload
       else
@@ -64,9 +27,13 @@ module Spree
       end
     end
 
+  end
+  Order.class_eval do
 
-    # changes line_item price value if user has a group discount
-    #
+    def user_group_adjustments
+      adjustments.where(:originator_type => "Spree::UserGroup")
+    end
+
     def update_prices_per_user
       return unless self.user.present?
 
@@ -84,5 +51,8 @@ module Spree
       self.reload if changes
       changes
     end
+
   end
 end
+
+Spree::Order.extend(Spree::UserGroupPricing)
