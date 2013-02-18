14e4b9675a606c0bd2893b92fb355c6e4a2c8a78
Rémi Gagnon <rgagnon@fungo.ca>
2013-02-17 19:44:57 -0500

    order decorator fix

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,25 +1,29 @@
 module Spree
-  # Placing these methods in module and calling super
-  # because alias_method was causing stack too deep errors:
-  # Bascially, methods were calling themselves recursively
-  # when running specs
-  module UserGroupPricing
+  Order.class_eval do
 
-    def new(*args)
-      super.extend(UserGroupPricing)
+    def user_group_adjustments
+      adjustments.where(:originator_type => "Spree::UserGroup")
     end
 
-    # ensure update_prices_per_user is called
-    # after every associate_user call
-    def associate_user!(user)
-      super
-      update_prices_per_user
-    end
-
-    # ensure update_prices_per_user is called
-    # after every add_varaint call
     def add_variant(variant, quantity = 1, currency = nil)
-      current_item = super
+      current_item = find_line_item_by_variant(variant)
+      if current_item
+        current_item.quantity += quantity
+        current_item.currency = currency unless currency.nil?
+        current_item.save
+      else
+        current_item = LineItem.new(:quantity => quantity)
+        current_item.variant = variant
+        if currency
+          current_item.currency = currency unless currency.nil?
+          current_item.price    = variant.price_in(currency).amount
+        else
+          current_item.price    = variant.price
+        end
+        self.line_items << current_item
+      end
+
+      self.reload
       if update_prices_per_user
         current_item.reload
       else
@@ -27,11 +31,15 @@ module Spree
       end
     end
 
-  end
-  Order.class_eval do
+    # Associates the specified user with the order.
+    def associate_user!(user)
+      self.user = user
+      self.email = user.email
+      # disable validations since they can cause issues when associating
+      # an incomplete address during the address step
+      save(:validate => false)
 
-    def user_group_adjustments
-      adjustments.where(:originator_type => "Spree::UserGroup")
+      update_prices_per_user
     end
 
     def update_prices_per_user
@@ -54,5 +62,3 @@ module Spree
 
   end
 end
-
-Spree::Order.extend(Spree::UserGroupPricing)
